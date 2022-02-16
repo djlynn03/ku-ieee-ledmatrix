8687dd4e0375eb411681c4bdd01c8ee5a21afabc
Duncan <djlynn03@gmail.com>
2022-02-16 17:42:19 -0600

    init

---
+++ bindings/python/samples/image-viewer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import time
+import sys
+import os
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image, ImageEnhance
+
+if len(sys.argv) < 2:
+    sys.exit("Require an image argument")
+else:
+    image_file = sys.argv[1]
+image = Image.open(image_file)
+
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 32
+options.chain_length = 1
+options.parallel = 1
+options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+
+matrix = RGBMatrix(options = options)
+
+# Make image fit our screen.
+image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+enhance = ImageEnhance.Contrast(image)
+image = enhance.enhance(3)
+#save_dir = '/home/pi/Desktop/rpi-rgb-led-matrix/rpi-rgb-led-matrix/bindings/python/samples'
+matrix.SetImage(image.convert('RGB'))
+#image.save(os.path.join(save_dir, 'test.png'))
+try:
+    print("Press CTRL-C to stop.")
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    sys.exit(0)
